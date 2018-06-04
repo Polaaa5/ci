6a97c3d6fcb8fe769a72e2aaff2be7b7cb5b568e
Polaaa5 <32522839+Polaaa5@users.noreply.github.com>
2018-06-04 23:17:45 +0500

    работа

---
+++ переворот строки.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void perevorot(char *s, int n);
+
+int main(void)
+{
+	char str[] = "polina";
+	int len = strlen(str);
+	char *str2;
+	str2 = (char*)calloc(len+1, sizeof(char));
+	strcpy(str2, str);
+	perevorot(str2, len);
+	printf("%s", str2);
+	free(str2);
+	return 0;
+}
+void perevorot(char *s, int n)
+{
+	for (int i = 0; i<n / 2; i++)
+	{
+		char t = s[i];
+		s[i] = s[n - i - 1];
+		s[n - i - 1] = t;
+	}
+}
